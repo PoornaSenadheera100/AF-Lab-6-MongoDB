b680e8831352cb78ff7cd0d024ae7ab2b918f257
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-03-25 09:56:10 +0530

    Added routes

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,35 @@ app.get('/api/students', async(req, res)=>{
     res.status(200).json(student);
 });
 
+app.post('/api/students', async(req, res)=>{
+    const student = new Student({
+        name: req.body.name,
+        studentId: req.body.studentId
+    });
+    await student.save();
+    res.status(201).json(student);
+});
+
+app.get('/api/students/:id', async(req, res)=>{
+    const id = req.params.id;
+    const student = await Student.findById(id);
+    res.status(200).json(student);
+});
+
+app.put('/api/students/:id', async(req, res)=>{
+    const id = req.params.id;
+    const student = await Student.findByIdAndUpdate(id, {
+        name: req.body.name,
+        studentId: req.body.studentId
+    }, {new: true});
+    res.status(200).json(student);
+});
+
+app.delete('/api/students/:id', async(req, res)=>{
+    const student = await Student.findByIdAndDelete(id);
+    res.status(204).json({message: 'Student deleted'});
+});
+
 app.listen(port, ()=>{
     console.log(`Sever is up and running on port ${port}`);
 });
